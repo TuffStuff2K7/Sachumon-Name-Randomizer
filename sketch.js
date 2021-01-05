01ff42a3d02543af84693fa174f9b792f89a2ee9
TuffStuff2K7 <76556622+TuffStuff2K7@users.noreply.github.com>
2021-01-05 11:26:22 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
 var sachumon_name = ["Sachumon", "Sachinovich Graphitius Iaroslav", "Sachino Pablo Escobar", "Sachuquedas Filipino Ramos", "Sachudananda", "Sachinese Inping", "Sachivitch Iustchiwitz", "Sache De Auditore", "Sachiver Hamilton", "potato"]
 
-var rand = Math.floor(Math.random() * 9);
+var rand = Math.floor(Math.random() * 10);
 
 var sachumon_todaysName = sachumon_name[rand];
 
